8a42e661ef27ba8b9b7241bb517f8aa9c2fedbbf
Danii <77706636+danielg9611@users.noreply.github.com>
2025-05-26 11:02:53 +0200

    update: hooks

--- app/app_model_hooks.py
+++ app/app_model_hooks.py
@@ -19,7 +19,7 @@ app.config["DEBUG"] = True
 # Enruta la landing page (endpoint /)
 @app.route("/", methods=["GET"])
 def hello():
-    return "Bienvenido a mi API del modelo advertising  :)"
+    return "Bienvenido a mi API del modelo advertising  :))"
 
 
 # Enruta la funcion al endpoint /api/v1/predict
@@ -83,8 +83,8 @@ def retrain():  # Rutarlo al endpoint '/api/v1/retrain/', metodo GET
 @app.route("/webhook", methods=["POST"])
 def webhook():
     # Ruta al repositorio donde se realizará el pull
-    path_repo = "/ruta/a/tu/repositorio/en/PythonAnywhere"
-    servidor_web = "/ruta/al/fichero/WSGI/de/configuracion"
+    path_repo = "/home/danielg9611/app-github/my-flask-app/app"
+    servidor_web = "/var/www/danielg9611_pythonanywhere_com_wsgi.py"
 
     # Comprueba si la solicitud POST contiene datos JSON
     if request.is_json:
